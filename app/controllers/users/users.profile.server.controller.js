e81a654d6d49f36ed7490771cf8314563775daa6
adidoo <adrien@mango.tools>
2014-10-06 17:32:24 +0200

    fix

--- app/controllers/users/users.profile.server.controller.js
+++ app/controllers/users/users.profile.server.controller.js
@@ -14,35 +14,52 @@ var _ = require('lodash'),
  */
 exports.update = function(req, res) {
 	// Init Variables
-	var user = req.user;
-	// For security measurement we remove the roles from the req.body object
-	delete req.body.roleTitle;
-    delete req.body.roleBitMask;
-	if (user) {
-		// Merge existing user
-		user = _.extend(user, req.body);
-		user.updated = Date.now();
-        user.displayName = user.firstName + ' ' + user.lastName;
-        user.save().done(function(err){
+	var user = db.User.build(req.user);
+
+    if(!req.user){
+        res.status(400).send({
+            message: 'User is not signed in'
+        });
+    }
+    else{
+        db.User.find({where: { id: req.user.id } }).done(function(err, user) {
             if (err) {
                 return res.status(400).send({
                     message: errorHandler.getErrorMessage(err)
                 });
-            } else {
-                req.login(user, function(err) {
-                    if (err) {
-                        res.status(400).send(err);
-                    } else {
-                        res.jsonp({user: user, token: tokenService.issueToken(user)});
-                    }
+            }
+            if (!user) {
+                return res.status(400).send({
+                    message: errorHandler.getErrorMessage(new Error('Failed to load user ' + id))
                 });
             }
+
+            // For security measurement we remove the roles from the req.body object
+            delete req.body.roleTitle;
+            delete req.body.roleBitMask;
+
+            // Merge existing user
+            user = _.extend(user, req.body);
+            user.updated = Date.now();
+            user.displayName = user.firstName + ' ' + user.lastName;
+            user.save().done(function (err) {
+                if (err) {
+                    return res.status(400).send({
+                        message: errorHandler.getErrorMessage(err)
+                    });
+                } else {
+                    req.login(user, function (err) {
+                        if (err) {
+                            res.status(400).send(err);
+                        } else {
+                            res.jsonp({user: user, token: tokenService.issueToken(user)});
+                        }
+                    });
+                }
+            });
         });
-	} else {
-		res.status(400).send({
-			message: 'User is not signed in'
-		});
-	}
+    }
+
 };
 
 /**
